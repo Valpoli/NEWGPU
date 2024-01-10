56bfd5e21fe2e21ff39d48fd572602f98b455d3b
Valentin Poli <valentin.poli@epita.fr>
2024-01-10 10:17:47 +0100

    yes

--- GPU/partiel/sources/ex1.cu
+++ GPU/partiel/sources/ex1.cu
@@ -17,8 +17,26 @@ void kernel_conv1(int* x, int* y, int N, int M, int* z)
 
 std::vector<int> conv1(const std::vector<int>& x, const std::vector<int>& y)
 {
-    // ...
-    return {};
+    int *x_GPU;
+    int *y_GPU;
+    cudaMemcpy(x_GPU, x.data(), x.size() * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(y_GPU, y.data(), y.size() * sizeof(int), cudaMemcpyHostToDevice);
+    int *res_GPU;
+    cudaMalloc(&res_GPU, x.size() * sizeof(int));
+
+    const dim3 threads_per_block(BLOCK_SIZE,1,1);
+
+    const dim3 blocks((x.size()/BLOCK_SIZE) + 1, 1, 1);
+
+    kernel_conv1<<<blocks,threads_per_block>>>(x_GPU,y_GPU,x.size(),y.size(),res_GPU);
+    std::vector<int> res(x.size());
+
+    cudaMemcpy(res, res_GPU, x.size() * sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaFree(x_GPU);
+    cudaFree(y_GPU);
+    cudaFree(res_GPU);
+    return res;
 }
 
 
