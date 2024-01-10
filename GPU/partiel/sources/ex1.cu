696e25ef532205b9f73dbe06f9faedc716105ecd
Valentin Poli <valentin.poli@epita.fr>
2024-01-10 10:33:12 +0100

    yes

--- GPU/partiel/sources/ex1.cu
+++ GPU/partiel/sources/ex1.cu
@@ -12,7 +12,7 @@ void kernel_conv1(int* x, int* y, int N, int M, int* z)
             total += y[i+k] * x[i+k];
         }
      }
-    z[i] = total;
+    z[i] = 777;
 }
 
 std::vector<int> conv1(const std::vector<int>& x, const std::vector<int>& y)
